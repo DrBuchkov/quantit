3a876d4b1ad73c35da709a3170122ac5bf427163
Nikolas Pafitis <nikolas.p@trgint.com>
2020-07-16 00:33:21 +0300

    Fixed defcomponent

--- src/clojure/quantit/component.clj
+++ src/clojure/quantit/component.clj
@@ -8,14 +8,6 @@
 (defn class->kw [class]
   (csk/->kebab-case-keyword class))
 
-(defn deps->map [deps]
-  (into {} (mapv (fn [[first second]]
-                   [first (class->kw second)])
-                 deps)))
-
-(defn deps->props [deps]
-  (mapv (comp symbol name first) deps))
-
 (defn constr-sym [name]
   (if (some? (namespace name))
     (symbol (str (namespace name) "/new-" name))
@@ -29,14 +21,13 @@
 (defmacro defcomponent [name depsv type & body]
   {:pre [(s/valid? symbol? name)
          (s/valid? ::deps depsv)]}
-  (let [props (conj (deps->props depsv) 'state)
+  (let [props (conj (map symbol depsv) 'state)
         map-constr (map-constr-sym name)
         constr (constr-sym name)]
     `(do
        (defrecord ~name ~props
          component/Lifecycle
          ~'(start [this] this)
-         ;(~'start [~'this] (assoc ~'this :deps ~(deps->map depsv)))
          ~'(stop [this] this))
        (extend-type ~name
          ~type
