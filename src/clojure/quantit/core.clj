ca507661893f7e0563be5c60aedca6003434a2c9
Nikolas Pafitis <nikolas.p@trgint.com>
2020-08-06 16:31:02 +0300

    Requiring spec namespaces in core.clj

--- src/clojure/quantit/core.clj
+++ src/clojure/quantit/core.clj
@@ -1,2 +1,11 @@
 (ns quantit.core
-  (:require [quantit.adapter.spec :refer :all]))
+  (:require
+    ; Specs
+    [quantit.adapter.spec :refer :all]
+    [quantit.backtest.spec :refer :all]
+    [quantit.bar.spec :refer :all]
+    [quantit.component.spec :refer :all]
+    [quantit.indicator.spec :refer :all]
+    [quantit.order.spec :refer :all]
+    [quantit.strategy.spec :refer :all]
+    [quantit.trade-system.spec :refer :all]))
