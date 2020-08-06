c4628faf6b5889130c25ac84da54c44d6bdf5187
Nikolas Pafitis <nikolas.p@trgint.com>
2020-08-06 23:03:44 +0300

    Improved quantit.component.spec

--- src/clojure/quantit/component/spec.clj
+++ src/clojure/quantit/component/spec.clj
@@ -7,11 +7,11 @@
 (s/def :quantit.component/default-params map?)
 (s/def :quantit.component/default-init-state map?)
 
-(s/def :quantit.component/basis (s/cat :dependencies (s/? (s/cat :k #(= % :dependencies)
+(s/def :quantit.component/basis (s/cat :dependencies (s/? (s/cat :k #{:dependencies}
                                                                  :v :quantit.component/dependencies))
-                                       :default-params (s/? (s/cat :k #(= % :default-params)
+                                       :default-params (s/? (s/cat :k #{:default-params}
                                                                    :v :quantit.component/default-params))
-                                       :default-init-state (s/? (s/cat :k #(= % :default-init-state)
+                                       :default-init-state (s/? (s/cat :k #{:default-init-state}
                                                                        :v :quantit.component/default-init-state))))
 
 (s/def :quantit.component/type #{'quantit.indicator.core/Indicator 'quantit.strategy.core/Strategy})
